88d3812fa15ea358dd5b0fd3d5276354805817a2
Rebecca Turner <rbt@sent.as>
2023-02-09 16:24:39 -0800

    Add CI
    The basic workflow is:
    
    - Label PRs with `patch`, `minor`, or `major`
    - When PRs are merged, version numbers are bumped automatically and a
      new PR is opened, labeled `release`
    - When a `release` PR is merged, a release is cut and built and
      published automatically

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
         final: prev: {
           nix-your-shell = final.rustPlatform.buildRustPackage {
             pname = "nix-your-shell";
-            version = "1.0.0";
+            version = "0.1.0"; # LOAD-BEARING COMMENT. See: `.github/workflows/version.yaml`
 
             cargoLock = {
               lockFile = ./Cargo.lock;
